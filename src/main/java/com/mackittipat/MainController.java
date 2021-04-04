5c05459ffd8ceee992d33fc654a14b860def958c
Mac <mac.kittipat@gmail.com>
2021-04-04 11:26:14 +0700

    Add more API

--- src/main/java/com/mackittipat/MainController.java
+++ src/main/java/com/mackittipat/MainController.java
@@ -1,11 +1,16 @@
 package com.mackittipat;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 public class MainController {
@@ -17,8 +22,35 @@ public class MainController {
     private String name;
 
     @GetMapping("/")
-    public String hello() {
+    public String index() {
         log.info("Hello " + name);
         return "Hello " + name;
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        log.info("Hello");
+        return "Hello";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        log.info("greeting");
+        return "greeting";
+    }
+
+    @PostMapping("/hi")
+    public String hi() {
+        log.info("hi");
+        return "hi";
+    }
+
+    @GetMapping("/upload")
+    public String upload() {
+        log.info("Uploading ...");
+        String uuid = UUID.randomUUID().toString();
+        AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.AP_SOUTHEAST_1).build();
+        s3.putObject("mac-file-bucket", uuid, uuid);
+        return "OK";
     }
 }
